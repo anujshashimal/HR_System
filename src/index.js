ca7bc0de924dfc6544ed4bbc54d85d000a1f4084
unknown <anujshashimal456@gmail.com>
2020-02-13 11:44:46 +0530

    Users Registration And Login Backend done

--- src/index.js
+++ src/index.js
@@ -10,13 +10,54 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-//create a new user
-app.post('/createu', (req, res) => {
-    const user = new newUser(req.body)
-    user.save()
+//creating a new user
+app.post('/createu', async (req, res) => {
+     try{
+        const user = await new newUser(req.body)
+        user.save()
+     }catch(e) {
+        res.status(404).send()
+     }
 })
 
-//reading a new user
+app.get('/user', (req, res)=> {
+
+    newUser.find({}).then((user) => {
+        res.send(user)
+        console.log(req.body)
+        }).catch((e) => {
+    })
+})
+
+//fetching specific user details
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id
+    newUser.findById(id).then((user) => {
+        if(!user){
+            return res.send(404).send
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        return res.send(404).send
+    })
+})
+
+//update user using id
+
+app.patch('/user/:id', async (req, res) => {
+    try{
+        const user = await newUser.findByIdAndUpdate(req.params.id,  req.body, { new : true, runValidator : true})
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+
 
 
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,25 +2,6 @@ const express = require('express')
 const router = express.Router()
 const User = require('../model/users')
 
-// router.get('/h', (req, res) => {
-//     res.send('This is Router')
-// })
-
-// router.post('/h', (req, res)=> {
-//     res.send('This is Router')
-// })
-
-// router.post('/home', (req, res) => {
-//     const userDet = new Userss(req.body)
-//       userDet.save()
-//       .then(item => {
-          
-//     })
-//       .catch(err => {
-//           res.status(400).send("Unable to save to database");
-//       });
-//    });
-
    router.post('/users', (req, res) => {
     res.send('Testing!')
 
@@ -31,7 +12,6 @@ const User = require('../model/users')
     })
 
 
-    //create a new user
 
 module.exports = router
 
